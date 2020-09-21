6072b5b656b02a9c8215729e8b42bec0d50cf2b1
shortCircuit81 <54492585+shortCircuit81@users.noreply.github.com>
2020-09-22 01:20:00 +0530

    Array

---
+++ Sort -0-1-2.java
@@ -0,0 +1,41 @@
+    public static void sort(int[]a){
+           int lo = 0; 
+        int hi = a.length - 1; 
+        int mid = 0, temp = 0; 
+        while (mid <= hi) { 
+            switch (a[mid]) { 
+            case 0: { 
+                temp = a[lo]; 
+                a[lo] = a[mid]; 
+                a[mid] = temp; 
+                lo++; 
+                mid++; 
+                break; 
+            } 
+            case 1: 
+                mid++; 
+                break; 
+            case 2: { 
+                temp = a[mid]; 
+                a[mid] = a[hi]; 
+                a[hi] = temp; 
+                hi--; 
+                break; 
+            } 
+            } 
+        } 
+                
+        }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+       sort(arr);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
