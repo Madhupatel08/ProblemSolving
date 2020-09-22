64072fdcfc1f5de2dd38df3961eb7047ae851688
shortCircuit81 <54492585+shortCircuit81@users.noreply.github.com>
2020-09-22 17:53:11 +0530

    Array

--- Sort -0-1-2.java
+++ Sort -0-1-2.java
@@ -1,29 +1,25 @@
-    public static void sort(int[]a){
-           int lo = 0; 
-        int hi = a.length - 1; 
-        int mid = 0, temp = 0; 
-        while (mid <= hi) { 
-            switch (a[mid]) { 
-            case 0: { 
-                temp = a[lo]; 
-                a[lo] = a[mid]; 
-                a[mid] = temp; 
-                lo++; 
-                mid++; 
-                break; 
-            } 
-            case 1: 
-                mid++; 
-                break; 
-            case 2: { 
-                temp = a[mid]; 
-                a[mid] = a[hi]; 
-                a[hi] = temp; 
-                hi--; 
-                break; 
-            } 
-            } 
-        } 
+//////partitioning problem
+public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }   
+public static void sort(int[]a){
+        int i=0;// j to i is 1
+    int j=0;//0 to j-1 is 0's 
+    int k=arr.length-1;//k+1 to end is 2's
+    ///i to k is unknown
+    while(i<=k){
+        if(arr[i]==0){
+            swap(arr,i,j);
+            i++;j++;
+        }else if(arr[i]==1){
+            i++;
+        }else{
+            swap(arr,i,k);
+            k--;
+        }
+    } 
                 
         }
     public static void main(String[] args) {
